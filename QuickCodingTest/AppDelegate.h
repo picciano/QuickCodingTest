a1c4a4d3b9dc5b207c6d527d89ffd6d86d8dff5b
Anthony Picciano <picciano1@Anthonys-MacBook-Pro.local>
2015-02-02 06:16:32 -0700

    Initial Commit

---
+++ QuickCodingTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QuickCodingTest
+//
+//  Created by Anthony Picciano on 2/2/15.
+//  Copyright (c) 2015 Anthony Picciano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QuickCodingTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QuickCodingTest
+//
+//  Created by Anthony Picciano on 2/2/15.
+//  Copyright (c) 2015 Anthony Picciano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
